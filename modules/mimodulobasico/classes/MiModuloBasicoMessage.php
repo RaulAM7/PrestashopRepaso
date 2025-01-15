1c4298061dcf27add77726728c12eb9b742fb19c
RaulAM <raulartilesm@gmail.com>
2025-01-15 11:39:30 +0000

    update

---
+++ modules/mimodulobasico/classes/MiModuloBasicoMessage.php
@@ -0,0 +1,27 @@
+<?php
+
+/* Esto es un Modelo de prueba para aprender a crear Modulos en Prestashop 
+
+Se encarga de guardar un mensaje en la base de datos y mostrarlo en el Front Office
+
+*/
+
+class MiModuloBasicoMessage extends ObjectModel
+{
+    public $id_mimodulobasico; // La ID de mi modulo
+    public $message; // El mensaje que quiero guardar
+
+    // Definir la estrucutra de la tabla
+    public static $definition = [
+        'table' => 'mimodulobasico_message',
+        'primary' => 'id_mimodulobasico',
+        'fields' => [
+            'message' => [
+                'type' => self::TYPE_STRING,
+                'validate' => 'isString',
+                'required' => true,
+                'size' => 255,
+            ],
+        ],
+    ];
+}
